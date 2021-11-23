e0d0cbadc83e8702669b416d6511672e13e1b8ae
Hardsty1e <59625228+Hardsty1e@users.noreply.github.com>
2021-11-23 16:16:24 +0530

    Practice file for web scraping

---
+++ WebScraper_realWebsite.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests as req
+
+html_text = req.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+soup = BeautifulSoup(html_text, 'lxml')
+jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+for job in jobs:
+    published_date = job.find('span', 'sim-posted').span.text
+    if 'few' in published_date:
+        company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
+        skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
+        
+        print(f'''
+        Company name: {company_name}
+        Required skills: {skills}
+        ''')
+
+        print('')
